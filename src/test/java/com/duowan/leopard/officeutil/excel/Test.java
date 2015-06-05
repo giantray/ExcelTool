6c50070d716547c2174b08c9a14798e4ba48b65a
lizeyang <lizeyang@yy.com>
2015-06-05 10:27:07 +0800

    优化注释

--- src/test/java/com/duowan/leopard/officeutil/excel/Test.java
+++ src/test/java/com/duowan/leopard/officeutil/excel/Test.java
@@ -21,19 +21,26 @@ public class Test extends TestCase {
 	public void testExport() throws WriteException {
 		long start = System.currentTimeMillis();
 
+		// 这个list,表示excel中的每一行数据
 		List<Object> li = new ArrayList<Object>();
+
+		// 在本例子中，每一行的数据，填充的是每个TestBean类的数据
 		TestBean testBean = new TestBean();
+		// 每一行有三列，分别是IntTest、StrTest，TimetTest
 		testBean.setIntTest(8888);
 		testBean.setStrTest("88888.888");
 		testBean.setTimeTest(new Timestamp(System.currentTimeMillis()));
 		for (int i = 0; i < 1000; i++) {
 			li.add(testBean);
 		}
+
+		// 这里定义了列的先后顺序。按照put顺序不同，第一列是填充timeTest属性，列的标题显示为time类型。依次类推是第二列、第三列
 		LinkedHashMap<String, String> keyMap = new LinkedHashMap<String, String>();
 		keyMap.put("timeTest", "time类型");
 		keyMap.put("intTest", "int类型");
 		keyMap.put("strTest", "string类型");
 
+		// 可以插入两个子表。并定义两个表的名字
 		List<ExportExcelBean> sheetContentList = new ArrayList<ExportExcelBean>();
 		ExportExcelBean bean1 = new ExportExcelBean();
 		bean1.setContentList(li);
@@ -50,9 +57,9 @@ public class Test extends TestCase {
 		File file = new File("d:/yy-export-excel");
 		file.mkdirs();
 
+		// 运行一下，查看输出的excel
 		try {
 			ExportExcelUtil util = new ExportExcelUtil();
-
 			OutputStream out = new FileOutputStream("d:/yy-export-excel/test.xls");
 			util.export(sheetContentList, out);
 		} catch (FileNotFoundException e) {
